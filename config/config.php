be25fe667a023714576552dd36cd0dfca15d9b2a
Alejandro [Workstation] <amberage.exe@gmail.com>
2024-04-03 22:38:57 -0600

    FINAL!! :D

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "";
+$mysql_username = "";
+$mysql_password = "";
+$dbname = "";
+
+//Estas variables se usan para poder leer las imagenes, ya que no es posible leerlas desde el rootPath
+$pathPicturesReplace = 'C:\Users\Alejandro\Desktop\App UPA'; //IMPORTANTE OMITIR EL ULTIMO / o \ SEGÚN SEA EL CASO DE LA RUTA.
+$pathChars = '';
+?>
